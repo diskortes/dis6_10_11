509c02f7d65cde39c4c773df8d4be7ca694222c6
I D <skazochnik@email.ua>
2011-10-06 16:25:42 +0300

    Added dis6_10_11

---
+++ src/dis6_10_11/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import patterns, include, url
+from dis6_10_11.views import hello, current_time, time_plus
+    
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', hello),
+    url(r'^time/$', current_time),
+    url(r'^time/plus/(\d{1,2})/$',time_plus),
+    # Examples:
+    # url(r'^$', 'dis6_10_11.views.home', name='home'),
+    # url(r'^dis6_10_11/', include('dis6_10_11.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ src/dis6_10_11/views.py
@@ -0,0 +1,24 @@
+'''
+Created on 06.10.2011
+
+@author: Server
+'''
+
+from django.http import HttpResponse, Http404
+import datetime
+
+def hello(request):
+    return HttpResponse("Hello DIS!")
+
+def current_time(request):
+    now ="<html><h2>Now %s</h2></html>" % datetime.datetime.now()
+    return HttpResponse(now)
+
+def time_plus(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now()+datetime.timedelta(hours=offset)
+    html = "After %s hours will be %s" % (offset, dt)
+    return HttpResponse(html)    
