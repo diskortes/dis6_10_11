d8dc289d439538a80d681dd08262c541f9c6d8b8
I D <skazochnik@email.ua>
2011-10-10 11:06:08 +0300

    Hm

--- src/dis6_10_11/books/admin.py
+++ src/dis6_10_11/books/admin.py
@@ -16,6 +16,9 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_date',)
     date_hierarchy = 'publication_date'
     ordering = ('-publication_date',)
+    fields = ('title','authors', 'publisher')
+    filter_horizontal = ('authors',)
+    raw_id_fields = ('publisher',)
 
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
